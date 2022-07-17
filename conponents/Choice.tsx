1b91809fca3532909700e0729d77073a41cd8a4a
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-17 22:44:34 +0200

    Add Choice component

---
+++ conponents/Choice.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { useContext } from "react";
+import { ScoreContext } from "../pages/_app";
+
+import Logo from "./logo";
+
+export default function Start() {
+
+  return (
+    <div className="choice">
+    </div>
+  );
+}
