5acfb60c087de95ea77061422fbb6dae5bee1b2e
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-18 12:33:42 +0200

    Handle Choice component

--- conponents/Choice.tsx
+++ conponents/Choice.tsx
@@ -1,13 +1,41 @@
+import { setUncaughtExceptionCaptureCallback } from "process";
 import * as React from "react";
 import { useContext } from "react";
 import { ScoreContext } from "../pages/_app";
+import Paper from "./image/Paper";
+import Rock from "./image/Rock";
+import Scissors from "./image/Scissors";
+import Triangle from "./image/Triangle";
 
-import Logo from "./logo";
-
-export default function Start() {
-
+export default function Choice() {
+  const { setChoice } = useContext(ScoreContext);
+  const handlePaper = () => {
+    setChoice("paper");
+  };
+  const handleRock=()=>{
+    setChoice("rock")
+  }
+  const handleScissors =()=>{
+    setChoice("scissors")
+  }
   return (
     <div className="choice">
+      <Triangle />
+      <div className="paper" onClick={handlePaper}>
+        <button className="btn paperColor">
+          <Paper />
+        </button>
+      </div>
+      <div className="rock">
+        <button onClick={handleRock} className="btn rockColor">
+          <Rock />
+        </button>
+      </div>
+      <div className="scissors">
+        <button onClick={handleScissors }className="btn scissorsColor">
+          <Scissors />
+        </button>
+      </div>
     </div>
   );
 }
