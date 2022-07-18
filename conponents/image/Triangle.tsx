381422064e2d8dc958ce3d9cc26e620557c666c6
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-18 12:33:11 +0200

    Add icon component

---
+++ conponents/image/Triangle.tsx
@@ -0,0 +1,13 @@
+export default function Triangle() {
+  return (
+    <svg width="313" height="278" xmlns="http://www.w3.org/2000/svg">
+      <path
+        stroke="#000"
+        strokeWidth="15"
+        fill="none"
+        opacity=".2"
+        d="M156.5 262 300 8H13z"
+      />
+    </svg>
+  );
+}
