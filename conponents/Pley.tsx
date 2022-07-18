d1a6a99de2f63115592355e22aff2bd2b4068861
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-18 13:41:35 +0200

    Add IconMaping

--- conponents/Pley.tsx
+++ conponents/Pley.tsx
@@ -1,5 +1,15 @@
+
 import { useContext } from "react";
 import { ScoreContext } from "../pages/_app";
+import Paper from "./image/Paper";
+import Rock from "./image/Rock";
+import Scissors from "./image/Scissors";
+
+export const IconMaping = {
+  paper: <Paper />,
+  rock: <Rock />,
+  scissors: <Scissors />,
+};
 
 export default function Play() {
   const { choice, setChoice } = useContext(ScoreContext);
@@ -8,7 +18,7 @@ export default function Play() {
   };
   return (
     <>
-      <h1> {choice} </h1>
+      {IconMaping[choice]}
       <button onClick={handleReturn}>return</button>
     </>
   );
