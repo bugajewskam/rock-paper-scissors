5f0030f3f8b8336d568f7508131e379eecbd65eb
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-20 16:07:18 +0200

    fixed context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,40 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { SetStateAction, useState, Dispatch } from "react";
 import { createContext } from "react";
 
-export type ChoiceType= "paper" | "rock" | "scissors";
-export const ScoreContext = createContext({
-  score: Number as unknown as number, choice: String as unknown as ChoiceType | null, setChoice:(choice:ChoiceType | null) =>{ },
-  computerChoice: String as unknown as string | null, setComputerChoice:(choice: string | null) =>{ }
+export type ChoiceType = "paper" | "rock" | "scissors";
+export type WinOrLost = "win" | "lost" | "remis" | "empty"
+export const ScoreContext = createContext<{
+    score: number,
+    choice: ChoiceType | null,
+    setChoice: (choice: ChoiceType | null) => void,
+    computerChoice: ChoiceType | null,
+    setComputerChoice: (choice: ChoiceType | null) => void,
+  winOrLost: WinOrLost ,
+  setWinOrLost: (result: WinOrLost) => void,
+  setScore: Dispatch<SetStateAction<number>>;
+}>({
+  score: 0,
+  choice: null,
+  setChoice:(x)=>{},
+  computerChoice: null,
+  setComputerChoice:(x)=>{},
+winOrLost: "empty" ,
+setWinOrLost:(result)=>{},
+setScore:()=>{}
 });
 function MyApp({ Component, pageProps }: AppProps) {
   const [score, setScore] = useState(9);
-  const [choice, setChoice]= useState<ChoiceType | null>(null)
-  const [computerChoice, setComputerChoice] = useState<string | null>(null)
+  const [choice, setChoice] = useState<ChoiceType | null>(null);
+  const [computerChoice, setComputerChoice] = useState<ChoiceType| null>(null);
+  const [winOrLost, setWinOrLost] = useState<WinOrLost>("empty");
+
 
   return (
-    <ScoreContext.Provider value={{ score , choice, setChoice, computerChoice,setComputerChoice}}>
+    <ScoreContext.Provider
+      value={{ score, choice, setChoice, computerChoice, setComputerChoice , winOrLost, setWinOrLost, setScore}}
+    >
       <Component {...pageProps} />
     </ScoreContext.Provider>
   );
